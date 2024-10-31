f592f1711d7d828abf1afde7d4d8ce5ffa6caabc
Drew Gifford <drew@drewgifford.com>
2024-10-31 09:41:15 -0400

    fix discord link

--- pages/index.vue
+++ pages/index.vue
@@ -10,7 +10,7 @@ import Container from '~/components/Container.vue';
             <h1 class="text-3xl text-center">
                 Unfortunately, we still need a little more time to develop LTElections.
             </h1>
-            <p class="text-2xl text-center mt-4 text-white">Check <a class="p-1 bg-gradient-to-r from-lte-blue/50 to-lte-red/50 rounded-sm" href="https:discord.gg/lte" target="_blank">discord.gg/lte</a> for website updates.</p>
+            <p class="text-2xl text-center mt-4 text-white">Check <a class="p-1 bg-gradient-to-r from-lte-blue/50 to-lte-red/50 rounded-sm" href="https://discord.gg/lte" target="_blank">discord.gg/lte</a> for website updates.</p>
 
             <p class="text-2xl text-center mt-12">Thank you for your patience! -Toadally</p>
         </div>
